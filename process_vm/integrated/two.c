daf947054a5a05aaf11a3ca836c0837f037ba2d4
Amir Hossein 00 <amirsorouri26@gmail.com>
2019-07-22 13:01:35 +0430

    it works as hell.

--- process_vm/integrated/two.c
+++ process_vm/integrated/two.c
@@ -10,7 +10,7 @@
 
 #include "psvm.h"
 
-#define PS_VM_ROW_SIZE     ((1ull) << 10)
+#define PS_VM_ROW_SIZE     ((1ull) << 11)
 #define PS_VM_COL_SIZE     ((1ull) << 20)
 
 const unsigned long int memSize = ( PS_VM_ROW_SIZE * PS_VM_COL_SIZE );
@@ -22,14 +22,14 @@ const unsigned long int memSize = ( PS_VM_ROW_SIZE * PS_VM_COL_SIZE );
  */
 int
 main() {
-        int fdOne,
-            fdTwo, 
-            nread;
+        int fd,
+            fdOne,
+            fdTwo;
         int pidPipe[2], 
             bufPipe[2];
         char *str, 
              **buf, 
-             **emptyBuf;
+             *emptyBuf;
         pid_t childPid, 
               childOnePid;
         struct timeval start, 
@@ -42,12 +42,9 @@ main() {
         fdTwo = open(FIFO_FILE_TWO, O_CREAT|O_TRUNC|O_WRONLY);
 
 	pid_t myPid;
-	emptyBuf = ipcb_fake_data_generator(PS_VM_ROW_SIZE, PS_VM_COL_SIZE);
-        // strcpy(emptyBuf[0], "end");
-        // emptyBuf[0][0] = 'e';
-        // emptyBuf[0][1] = 'n';
-        // emptyBuf[0][2] = '\0';
-        //printf("%s\n", emptyBuf[0]);
+	// emptyBuf = ipcb_fake_data_generator(PS_VM_ROW_SIZE, PS_VM_COL_SIZE);
+    	fd = ipcb_open_shm("mypsvm", memSize);
+   	emptyBuf = ipcb_map_memory_to_fd(memSize, fd, 0);
 
 	myPid = getpid();
         printf("MPID: %d, pointer: %p\n", myPid, emptyBuf);
@@ -64,4 +61,47 @@ void sighup()
 {
     signal(SIGHUP, sighup); /* reset signal */
     printf("CHILD: I have received a SIGHUP\n");
+}
+
+/*
+ *  Lorem Ipsum
+ */
+char*
+ipcb_map_memory_to_fd (unsigned long long memorySize, int fd, off_t offset) {
+    char* str;
+
+    str = mmap(NULL, 2ull * memorySize, PROT_READ|PROT_WRITE, MAP_SHARED, fd, offset);
+    if (MAP_FAILED == str) ipcb_print_error("ipcb_map_memory_to_fd: mmap");
+    return str;
+}
+
+
+/*
+ *  Lorem Ipsum
+ */
+int
+ipcb_unmapp_memory (void *addr, size_t length) {
+    int res;
+
+    res = munmap(addr, length);
+    if (0 != res) ipcb_print_error("ipcb_unmapp_memory:munmap");
+    return res;
+}
+
+
+/*
+ *  Lorem Ipsum
+ */
+int
+ipcb_open_shm (const char* name, unsigned long long truncationSize) {
+    int fd;
+    
+    fd = shm_open(name, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    if (fd == -1) ipcb_print_error("ipcb_open_shm:shm_open");
+    
+    if((ftruncate(fd, 10ull * truncationSize)<0)) {
+            printf("Ftruncate failed\n");
+            return -1;
+    }
+    else{ return fd; }
 }
