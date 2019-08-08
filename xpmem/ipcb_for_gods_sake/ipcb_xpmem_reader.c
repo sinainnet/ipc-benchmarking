1b6fefcfe12963916bdd321c3d68dec4517f93dc
Amir Hossein 00 <amirsorouri26@gmail.com>
2019-08-08 23:44:26 +0430

    forget _bench version and starting to implement something new how it'll work soon.

---
+++ xpmem/ipcb_for_gods_sake/ipcb_xpmem_reader.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include "sem.h"
+
+#include "../../commons/commons.h"
+
+
+int 
+main(int argc, char **argv) {
+    // if (argc < 2) {
+	// 	printf("In Writer: Usage: %s <test number>\n", argv[0]);
+	//   	return -1;
+	// }
+	// char *name = atoi(argv[1]);
+    int lock;
+    if ((lock = open("/tmp/xpmem.lock", O_RDWR | O_CREAT)) == -1) {
+		perror("open xpmem.lock");
+		return -1;
+	}
+    lockf(lock, F_LOCK, 0);
+    int id = semget(KEY, 1, 0666 | IPC_CREAT);
+    if(id < 0)
+    {
+            perror("semget"); exit(11);
+    }
+    union semun u;
+    u.val = 0;
+    if(semctl(id, 0, SETVAL, u) < 0)
+    {
+            perror("semctl"); exit(12);
+    }
+    char *s = "ABCDEFGH";
+    int l = strlen(s);
+    if(semop(id, &p, 1) < 0){
+        perror("semop p"); exit(15);
+    }
+	printf("   \n\n==== Reader(Capital): STARTS ====\n");
+    for(int i = 0; i < l; ++i)
+    {
+        putchar(s[i]);
+        fflush(stdout);
+        // sleep(rand() % 2);
+        putchar(s[i]);
+        fflush(stdout);
+        // sleep(rand() % 2);
+    }
+    if(semop(id, &v, 1) < 0){
+        perror("semop p"); exit(16);
+    }
+	lockf(lock, F_ULOCK, 0);
+	printf("   \n\n==== Reader(Capital): Ends ====\n");
+    return 0;
+}
---
+++ xpmem/ipcb_for_gods_sake/run.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+test -e /tmp/xpmem.share && rm -f /tmp/xpmem.share
+test -e /tmp/xpmem.lock && rm -f /tmp/xpmem.lock
+test -e /dev/shm/sem.ipcb_semaphore_example && rm -f /dev/shm/sem.ipcb_semaphore_example
+
+# create TMP_SHARE_SIZE bytes defined in xpmem_test.h
+#for i in `seq 0 31` ; do
+#	echo -n 0 >> /tmp/ipcb_xpmem.share
+#done
+echo 0 > /tmp/xpmem.lock
+
+# Run the main test app
+$PWD/ipcb_xpmem_master
+
+# Tests complete -- now check for memory leaks
+#echo "==== test_mem_leak STARTS ===="
+#PINNED=$(grep "pages pinned" /proc/xpmem/* | sed -e 's/^.*: //')
+#UNPINNED=$(grep "pages unpinned" /proc/xpmem/* | sed -e 's/^.*: //')
+#echo "all pinned pages = $PINNED"
+#echo "all unpinned pages = $UNPINNED"
+#if [ "$PINNED" -eq "$UNPINNED" ]; then
+#	echo "==== test_mem_leak PASSED ===="
+#else
+#	echo "==== test_mem_leak FAILED ===="
+#fi
+
+if [ -e "/tmp/xpmem.share" ]; then
+	rm /tmp/xpmem.share
+fi
+if [ -e "/tmp/xpmem.lock" ]; then
+	rm /tmp/xpmem.lock
+fi
+if [ -e "/dev/shm/sem.ipcb_semaphore_example" ]; then
+	sudo rm /dev/shm/sem.ipcb_semaphore_example
+fi
