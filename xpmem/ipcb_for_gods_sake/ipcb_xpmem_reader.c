3b6672687045f3bd6fd8993f6ef912ccf8e57417
Amir Hossein 00 <amirsorouri26@gmail.com>
2019-08-10 13:12:41 +0430

    this is one of the most stable versions of having semaphore in the code and having priority on writer would be the first one to get access to the shm.

--- xpmem/ipcb_for_gods_sake/ipcb_xpmem_reader.c
+++ xpmem/ipcb_for_gods_sake/ipcb_xpmem_reader.c
@@ -23,22 +23,22 @@ main(int argc, char **argv) {
 		return -1;
 	}
     lockf(lock, F_LOCK, 0);
-    int id = semget(KEY, 1, 0666 | IPC_CREAT);
-    if(id < 0)
-    {
-            perror("semget"); exit(11);
-    }
-    union semun u;
-    u.val = 0;
-    if(semctl(id, 0, SETVAL, u) < 0)
-    {
-            perror("semctl"); exit(12);
-    }
+    // int id = semget(KEY, 1, 0666 | IPC_CREAT);
+    // if(id < 0)
+    // {
+    //         perror("semget"); exit(11);
+    // }
+    // union semun u;
+    // u.val = 1;
+    // if(semctl(id, 0, SETVAL, u) < 0)
+    // {
+    //         perror("semctl"); exit(12);
+    // }
     char *s = "ABCDEFGH";
     int l = strlen(s);
-    if(semop(id, &p, 1) < 0){
-        perror("semop p"); exit(15);
-    }
+    // if(semop(id, &p, 1) < 0){
+    //     perror("semop p"); exit(15);
+    // }
 	printf("   \n\n==== Reader(Capital): STARTS ====\n");
     for(int i = 0; i < l; ++i)
     {
@@ -49,9 +49,9 @@ main(int argc, char **argv) {
         fflush(stdout);
         // sleep(rand() % 2);
     }
-    if(semop(id, &v, 1) < 0){
-        perror("semop p"); exit(16);
-    }
+    // if(semop(id, &v, 1) < 0){
+    //     perror("semop p"); exit(16);
+    // }
 	lockf(lock, F_ULOCK, 0);
 	printf("   \n\n==== Reader(Capital): Ends ====\n");
     return 0;
