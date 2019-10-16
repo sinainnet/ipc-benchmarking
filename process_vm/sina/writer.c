c0c18929cb2d6a883a27147ad586b997b56efe9c
Amir Hossein <amirsorouri26@gmail.com>
2019-10-16 19:49:58 +0330

    process_vm tested on the viper system and result is shown on the readme file.

--- process_vm/sina/writer.c
+++ process_vm/sina/writer.c
@@ -8,19 +8,35 @@
 int ipcb_test_base_one(test_args* t) { return 0; }
 
 
+
 int main()
 {
     union semun u, j;
 	u.val = 1;
     j.val = 0;
 
+    int mgrow = 1024;
+    int gigrow = 1048576;
+    long int two_gigrow = 2*gigrow;
+    long int three_gigrow = 3*gigrow;
+    long int four_gigrow = 4*gigrow;
+    long int eight_gigrow = 8*gigrow;
+    int col = 1024;
+
+    unsigned long int mgsize = mgrow * col;
+    unsigned long int gigsize = gigrow * col;
+    unsigned long int two_gigsize = two_gigrow * col;
+    unsigned long int three_gigsize = three_gigrow * col;
+    unsigned long int four_gigsize = four_gigrow * col;
+    unsigned long int eight_gigsize = eight_gigrow * col;
+
     int id_sem = ipcb_get_semaphore(shared_sem_key, 1, 0666 | IPC_CREAT);
     ipcb_control_semaphore(id_sem, 0, SETVAL, u);
     ipcb_operate_semaphore(id_sem, &decrease, 1);
 
-    char *data = calloc(1024, 1024);
-    memset(data, 'a', 8191);
-    printf("%d %p %u \n", getpid(), data, 1024 * 1024);
+    char *data = calloc(eight_gigrow, col);
+    memset(data, 'a', eight_gigsize);
+    printf("%d %p %lu \n", getpid(), data, eight_gigsize);
 
     // while (1);
     ipcb_operate_semaphore(id_sem, &increase, 1); 
