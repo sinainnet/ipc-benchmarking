00c5f9e576361888a7271ab0b25c723c8cfb0167
Amir Hossein Sorouri <amirsorouri26@gmail.com>
2019-12-12 12:08:23 +0330

    test if psvm accepts tid instead of pid and it worked.

---
+++ psvm/tid/reader.c
@@ -0,0 +1,84 @@
+#define _GNU_SOURCE
+#include <sys/uio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+int main(int argc, char **argv) {
+        if (argc < 3) {
+                printf("usage: %s <pid> <mem address> [len]\n", argv[0]);
+                printf("  <pid> - PID of process to target\n");
+                printf("  <mem> - Memory address to target\n");
+                printf("  [len] - Length (in bytes) to dump\n");
+                return -1;
+        }
+
+        // PARSE CLI ARGS
+        pid_t pid = strtol(argv[1], NULL, 10);
+        printf(" * Launching with a target PID of: %d\n", pid);
+
+        void *remotePtr = (void *)strtol(argv[2], NULL, 0);
+        printf(" * Launching with a target address of 0x%llx\n", (long long unsigned)remotePtr);
+
+        size_t bufferLength = (argc > 3) ? strtol(argv[3], NULL, 10) : 20;
+        printf(" * Launching with a buffer size of %lu bytes.\n", bufferLength);
+
+        // Build iovec structs
+        struct iovec local[1];
+        local[0].iov_base = calloc(bufferLength, sizeof(char));
+        local[0].iov_len = bufferLength;
+
+        struct iovec remote[1];
+        remote[0].iov_base = remotePtr;
+        remote[0].iov_len = bufferLength;
+
+        // Call process_vm_readv - handle any error codes
+
+        struct timespec start, finish;
+        clock_gettime(CLOCK_REALTIME, &start);
+        ssize_t nread = process_vm_readv(pid, local, 2, remote, 1, 0);
+        // ssize_t nread2 = process_vm_readv(pid, local, 2, remote, 1, 0);
+        clock_gettime(CLOCK_REALTIME, &finish);
+        if (nread < 0) {
+                switch (errno) {
+                        case EINVAL:
+                                printf("ERROR: INVALID ARGUMENTS.\n");
+                                break;
+                        case EFAULT:
+                                printf("ERROR: UNABLE TO ACCESS TARGET MEMORY ADDRESS.\n");
+                                break;
+                        case ENOMEM:
+                                printf("ERROR: UNABLE TO ALLOCATE MEMORY.\n");
+                                break;
+                        case EPERM:
+                                printf("ERROR: INSUFFICIENT PRIVILEGES TO TARGET PROCESS.\n");
+                                break;
+                        case ESRCH:
+                                printf("ERROR: PROCESS DOES NOT EXIST.\n");
+                                break;
+                        default:
+                                printf("ERROR: AN UNKNOWN ERROR HAS OCCURRED.\n");
+                }
+
+                return -1;
+        }
+
+        printf(" * Executed process_vm_ready, read %zd bytes.\n", nread);
+        printf("%s\n", (char *)(local[0].iov_base));
+
+        long seconds = finish.tv_sec - start.tv_sec;
+        long ns = finish.tv_nsec - start.tv_nsec;
+
+        if (start.tv_nsec > finish.tv_nsec) { // clock underflow
+                --seconds;
+                ns += 1000000000;
+        }
+        printf("seconds without ns: %ld\n", seconds);
+        printf("nanoseconds: %ld\n", ns);
+        printf("total seconds: %e\n", (double)seconds + (double)ns/(double)1000000000);
+
+        return 0;
+}
