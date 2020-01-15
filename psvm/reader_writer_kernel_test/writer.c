71a9bc915e4602c1cc3cf8c77a09f8db9e87bb26
Amir Hossein Sorouri <amirsorouri26@gmail.com>
2020-01-15 12:04:23 +0330

    reader-writer tests for kernel, busy-waiting and blocking developed.

--- psvm/reader_writer_kernel_test/writer.c
+++ psvm/reader_writer_kernel_test/writer.c
@@ -9,54 +9,28 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
+#include <sched.h>
 
-int main(int argc, char **argv)
-{
-        if (argc < 3) {
-                printf("usage: %s <pid> <mem address> [len]\n", argv[0]);
-                printf("  <pid> - PID of process to target\n");
-                printf("  <mem> - Memory address to target\n");
-                printf("  [len] - Length (in bytes) to dump\n");
-                return -1;
+void set_cpu_scheduler(int cpu_no, int priority) {
+        cpu_set_t set;
+        CPU_ZERO(&set);
+        CPU_SET(cpu_no, &set);
+        if (sched_setaffinity(getpid(), sizeof(set), &set) == -1)
+        {
+                printf("setaffinity error");
+                exit(0);
         }
+        struct sched_param sch_p;
+        sch_p.sched_priority = priority;
+        int re = sched_setscheduler(getpid(), SCHED_RR, &sch_p);
+        if(re < 0) {
+                printf("sched_setscheduler returned error code %d.\n", re);
+                exit(0);
+        }
+        return;
+}
 
-        // PARSE CLI ARGS
-        pid_t pid = strtol(argv[1], NULL, 10);
-        printf(" * Launching with a target PID of: %d\n", pid);
-
-        void *remotePtr = (void *)strtol(argv[2], NULL, 0);
-        printf(" * Launching with a target address of 0x%llx\n", (long long unsigned)remotePtr);
-
-        size_t bufferLength = (argc > 3) ? strtol(argv[3], NULL, 10) : 20;
-        printf(" * Launching with a buffer size of %lu bytes.\n", bufferLength);
-        
-        int mgrow = 1024;
-        int gigrow = 1048576;
-        long int two_gigrow = 2*gigrow;
-        int col = 1024;
-        unsigned long int mgsize = mgrow * col;
-        unsigned long int gigsize = gigrow * col;
-        unsigned long int two_gigsize = two_gigrow * col;
-
-        char *data = calloc(two_gigrow, col);
-        memset(data, 'a', two_gigsize);
-        printf("writer: %d %p %lu \n", getpid(), data, two_gigsize);
-
-        // Build iovec structs
-        struct iovec local[1];
-        local[0].iov_base = data;
-        local[0].iov_len = bufferLength;
-        
-        struct iovec remote[1];
-        remote[0].iov_base = remotePtr;
-        remote[0].iov_len = bufferLength;
-
-        // Call process_vm_readv - handle any error codes
-        struct timespec start, finish;
-        clock_gettime(CLOCK_REALTIME, &start);
-        ssize_t nread2 = process_vm_writev(pid, local, 2, remote, 1, 0);
-        clock_gettime(CLOCK_REALTIME, &finish);
-
+void psvm_error_handler(ssize_t nread2) {
         if (nread2 < 0) {
                 switch (errno) {
                         case EINVAL:
@@ -77,9 +51,62 @@ int main(int argc, char **argv)
                         default:
                                 printf("ERROR: AN UNKNOWN ERROR HAS OCCURRED.\n");
                 }
+                exit(1);
+        }
+}
 
+int main(int argc, char **argv)
+{
+        if (argc < 3) {
+                printf("usage: %s <pid> <mem address> [len]\n", argv[0]);
+                printf("  <pid> - PID of process to target\n");
+                printf("  <mem> - Memory address to target\n");
+                printf("  [len] - Length (in bytes) to dump\n");
                 return -1;
         }
+
+        set_cpu_scheduler(1, 99);
+
+        // PARSE CLI ARGS
+        pid_t pid = strtol(argv[1], NULL, 10);
+        printf(" * Launching with a target PID of: %d\n", pid);
+
+        void *remotePtr = (void *)strtol(argv[2], NULL, 0);
+        printf(" * Launching with a target address of 0x%llx\n", (long long unsigned)remotePtr);
+
+        size_t bufferLength = (argc > 3) ? strtol(argv[3], NULL, 10) : 20;
+        printf(" * Launching with a buffer size of %lu bytes.\n", bufferLength);
+        
+        int mgrow = 1024;
+        int gigrow = 1048576;
+        long int two_gigrow = 2*gigrow;
+        int col = 1024;
+        unsigned long int mgsize = mgrow * col;
+        unsigned long int gigsize = gigrow * col;
+        unsigned long int two_gigsize = two_gigrow * col;
+        
+        char *data = calloc(bufferLength, sizeof(char));
+        memset(data, 'a', gigsize);
+
+        // Build iovec structs
+        bufferLength = gigsize;
+        struct iovec local[1];
+        local[0].iov_base = data;
+        local[0].iov_len = bufferLength;
+        
+        struct iovec remote[1];
+        remote[0].iov_base = remotePtr;
+        remote[0].iov_len = bufferLength;
+
+        // Call process_vm_readv - handle any error codes
+        struct timespec start, finish;
+        clock_gettime(CLOCK_REALTIME, &start);
+
+        ssize_t nread2 = process_vm_writev(pid, local, 2, remote, 1, 0);
+
+        clock_gettime(CLOCK_REALTIME, &finish);
+
+        psvm_error_handler(nread2);
 
         printf(" * Executed process_vm_ready, read %zd bytes.\n", nread2);
 
