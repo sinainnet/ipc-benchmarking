1ad13a5acd72cec311e2649ee9cf71d454b62b26
Amir Hossein Sorouri <amirsorouri26@gmail.com>
2020-01-26 19:59:51 +0330

    revisions on implementation by using transblocklibbench and testing script to calculate the results stored in file.txt, tested and it all works fine.

--- psvm/reader_writer_test_block/reader.c
+++ psvm/reader_writer_test_block/reader.c
@@ -1,53 +1,17 @@
 #define _GNU_SOURCE
-#include <sys/uio.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <time.h>
-#include <sys/time.h>
-#include <sys/resource.h>
-#include <sys/types.h>
 #include <unistd.h>
-#include <sched.h>
-#include <sched.h>
 #include <string.h>
+#include <sys/uio.h>
+#include <sys/resource.h>
+#include "../header.h"
 #include "helper.h"
 
-void set_cpu_scheduler(int cpu_no, int priority) {
-        cpu_set_t set;
-        CPU_ZERO(&set);
-        CPU_SET(cpu_no, &set);
-        if (sched_setaffinity(getpid(), sizeof(set), &set) == -1)
-        {
-                printf("setaffinity error");
-                exit(0);
-        }
-        struct sched_param sch_p;
-        sch_p.sched_priority = priority;
-        int re = sched_setscheduler(getpid(), SCHED_RR, &sch_p);
-        if(re < 0) {
-                printf("sched_setscheduler returned error code %d.\n", re);
-                exit(0);
-        }
-        return;
-}
-
 int main(int argc, char **argv) {
-
-        int mgrow = 1024;
-        int gigrow = 1048576;
-        long int two_gigrow = 2*gigrow;
-        long int three_gigrow = 3*gigrow;
-        long int four_gigrow = 4*gigrow;
-        long int eight_gigrow = 8*gigrow;
-        int col = 1024;
-
-        unsigned long int mgsize = mgrow * col;
-        unsigned long int gigsize = gigrow * col;
-        unsigned long int two_gigsize = two_gigrow * col;
-        unsigned long int three_gigsize = three_gigrow * col;
-        unsigned long int four_gigsize = four_gigrow * col;
-        unsigned long int eight_gigsize = eight_gigrow * col;
+        // Changing the process scheduling queue into real-time and set its priority using <sched.h>.
+        set_cpu_scheduler(2,99);
+        
         union semun u, j;
 	u.val = 1;
         j.val = 0;
@@ -55,18 +19,8 @@ int main(int argc, char **argv) {
         // Changing the process scheduling queue into real-time and set its priority using <sched.h>.
         set_cpu_scheduler(2,99);
 
-        char *data = calloc(four_gigrow, col);
-        printf("reader: sudo ./writer %d %p %lu \n", getpid(), data, four_gigsize);
-
-        // Build iovec structs
-        size_t bufferLength = four_gigsize;
-        struct iovec local[1];
-        local[0].iov_base = data;
-        local[0].iov_len = bufferLength;
-
-        struct iovec remote[1];
-        remote[0].iov_base = calloc(bufferLength, sizeof(char));;
-        remote[0].iov_len = bufferLength;
+        char *data = calloc(gig_row, col);
+        printf("reader: sudo ./writer %d %p %lu \n", getpid(), data, gig_size);
 
         int id_wrt = ipcb_get_semaphore(shared_wrt_key, 1, 0666 | IPC_CREAT);
         ipcb_control_semaphore(id_wrt, 0, SETVAL, j);
--- psvm/reader_writer_test_block/writer.c
+++ psvm/reader_writer_test_block/writer.c
@@ -2,159 +2,65 @@
 #include <sys/uio.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <time.h>
-#include <sys/time.h>
 #include <sys/resource.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
-#include <sched.h>
 #include "helper.h"
+#include "../header.h"
 
-void set_cpu_scheduler(int cpu_no, int priority) {
-        cpu_set_t set;
-        CPU_ZERO(&set);
-        CPU_SET(cpu_no, &set);
-        if (sched_setaffinity(getpid(), sizeof(set), &set) == -1)
-        {
-                printf("setaffinity error");
-                exit(0);
-        }
-        struct sched_param sch_p;
-        sch_p.sched_priority = priority;
-        int re = sched_setscheduler(getpid(), SCHED_RR, &sch_p);
-        if(re < 0) {
-                printf("sched_setscheduler returned error code %d.\n", re);
-                exit(0);
-        }
-        return;
-}
-
-void psvm_error_handler(ssize_t nread2) {
-        if (nread2 < 0) {
-                switch (errno) {
-                        case EINVAL:
-                                printf("ERROR: INVALID ARGUMENTS.\n");
-                                break;
-                        case EFAULT:
-                                printf("ERROR: UNABLE TO ACCESS TARGET MEMORY ADDRESS.\n");
-                                break;
-                        case ENOMEM:
-                                printf("ERROR: UNABLE TO ALLOCATE MEMORY.\n");
-                                break;
-                        case EPERM:
-                                printf("ERROR: INSUFFICIENT PRIVILEGES TO TARGET PROCESS.\n");
-                                break;
-                        case ESRCH:
-                                printf("ERROR: PROCESS DOES NOT EXIST.\n");
-                                break;
-                        default:
-                                printf("ERROR: AN UNKNOWN ERROR HAS OCCURRED.\n");
-                }
-                exit(1);
-        }
-}
-
-int main(int argc, char **argv)
-{
-        if (argc < 3) {
-                printf("usage: %s <pid> <mem address> [len]\n", argv[0]);
-                printf("  <pid> - PID of process to target\n");
-                printf("  <mem> - Memory address to target\n");
-                printf("  [len] - Length (in bytes) to dump\n");
-                return -1;
-        }
-
+int main(int argc, char **argv) {
         set_cpu_scheduler(1, 99);
 
         // PARSE CLI ARGS
-        pid_t pid = strtol(argv[1], NULL, 10);
-        printf(" * Launching with a target PID of: %d\n", pid);
-
-        void *remotePtr = (void *)strtol(argv[2], NULL, 0);
-        printf(" * Launching with a target address of 0x%llx\n", (long long unsigned)remotePtr);
-
-        size_t bufferLength = (argc > 3) ? strtol(argv[3], NULL, 10) : 20;
-        printf(" * Launching with a buffer size of %lu bytes.\n", bufferLength);
-
-        int mgrow = 1024;
-        int gigrow = 1048576;
-        long int two_gigrow = 2*gigrow;
-        int col = 1024;
-        unsigned long int mgsize = mgrow * col;
-        unsigned long int gigsize = gigrow * col;
-        unsigned long int two_gigsize = two_gigrow * col;
-
-
-        char *data1 = calloc(bufferLength, sizeof(char));
-        memset(data1, 'a', bufferLength);
-
-        char *data2 = calloc(bufferLength, sizeof(char));
-        memset(data2, 'b', bufferLength);
-
-        char *data3 = calloc(bufferLength, sizeof(char));
-        memset(data3, 'c', bufferLength);
-
-        char *data4 = calloc(bufferLength, sizeof(char));
-        memset(data4, 'd', bufferLength);
+        data_input inputs;
+        get_inputs(&inputs, argc, argv);
 
         // Build iovec structs
-        bufferLength = gigsize;
-        struct iovec local1[1];
-        local1[0].iov_base = data1;
-        local1[0].iov_len = bufferLength;
+        int local_iov_num = inputs.buffer_length / gig_size;
+        struct iovec local[local_iov_num];
+        for (int i = 0; i < local_iov_num; i++)
+        {
+                char *data = calloc(inputs.buffer_length, sizeof(char));
+                memset(data, 'a' + i, gig_size);
+                local[i].iov_base = data;
+                local[i].iov_len = gig_size;
+        }
 
-        struct iovec local2[1];
-        local2[0].iov_base = data2;
-        local2[0].iov_len = bufferLength;
-
-        struct iovec local3[1];
-        local3[0].iov_base = data3;
-        local3[0].iov_len = bufferLength;
-
-        struct iovec local4[1];
-        local4[0].iov_base = data4;
-        local4[0].iov_len = bufferLength;
-        
         struct iovec remote[1];
-        remote[0].iov_base = remotePtr;
-        remote[0].iov_len = bufferLength;
+        remote[0].iov_base = inputs.remote_ptr;
+        remote[0].iov_len = inputs.buffer_length;
+
 
         // Call process_vm_readv - handle any error codes
         // int id_srt = ipcb_get_semaphore(shared_sem_key, 1, 0666);
         
+        struct timespec start, finish;
+        ssize_t nread[local_iov_num];
+        ssize_t nread_res = 0;
+        memset(nread, 0, local_iov_num*sizeof(ssize_t));
         int id_wrt = ipcb_get_semaphore(shared_wrt_key, 1, 0666); // key to inform reader.
 
-        ssize_t nread1, nread2 = 0, nread3 = 0, nread4 = 0;
-
-        struct timespec start, finish;
         clock_gettime(CLOCK_REALTIME, &start);
 
         // ipcb_operate_semaphore(id_srt, &decrease, 1);
-         nread1 = process_vm_writev(pid, local1, 2, remote, 1, 0);
-         nread2 = process_vm_writev(pid, local2, 2, remote, 1, 0);
-         nread3 = process_vm_writev(pid, local3, 2, remote, 1, 0);
-         nread4 = process_vm_writev(pid, local4, 2, remote, 1, 0);
+        for (int i = 0; i < local_iov_num; i++)
+        {
+                nread[i] = process_vm_writev(inputs.pid, (struct iovec *)&local[i], 1, remote, 1, 0);
+                nread_res+=nread[i];
+        }
         // ipcb_operate_semaphore(id_srt, &increase, 1);
         ipcb_operate_semaphore(id_wrt, &increase, 1);
 
         clock_gettime(CLOCK_REALTIME, &finish);
 
-        psvm_error_handler(nread2);
-
-        printf(" * Executed process_vm_ready, read %zd bytes.\n", nread1 + nread2 + nread3 + nread4);
-
-        long seconds = finish.tv_sec - start.tv_sec;
-        long ns = finish.tv_nsec - start.tv_nsec;
-
-        if (start.tv_nsec > finish.tv_nsec) { // clock underflow
-                --seconds;
-                ns += 1000000000;
+        // Error Checking
+        for (int i = 0; i < local_iov_num; i++)
+        {
+                if(psvm_error_handler(nread[i]) < 0)
+                        exit(1);
         }
-        printf("seconds without ns: %ld\n", seconds);
-        printf("nanoseconds: %ld\n", ns);
-        printf("total seconds: %e\n", (double)seconds + (double)ns/(double)1000000000);
 
+        // Print Result
+        print_results(psvm_writer, nread_res, start, finish);
         return 0;
 }
