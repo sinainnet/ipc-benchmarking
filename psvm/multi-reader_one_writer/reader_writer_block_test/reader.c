7252af800d59d81e84c271462cfdfbfce4b8fecf
Amir Hossein Sorouri <amirsorouri26@gmail.com>
2020-01-27 19:54:58 +0330

    multithreaded blocking test implemented but semaphore not work as i wish. There is still some work to be done on semaphore management ...

---
+++ psvm/multi-reader_one_writer/reader_writer_block_test/reader.c
@@ -0,0 +1,30 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/uio.h>
+#include <sys/resource.h>
+#include "../../header.h"
+#include "../../helper.h"
+
+int main(int argc, char **argv) {
+        // Changing the process scheduling queue into real-time and set its priority using <sched.h>.
+        set_cpu_scheduler(2,99);
+        
+        union semun u, j;
+	// u.val = 1;
+        j.val = 0;
+
+        char *data = calloc(two_gig_row, col);
+        printf("reader: sudo ./writer %d %p %lu \n", getpid(), data, two_gig_size);
+
+        int id_wrt = ipcb_get_semaphore(shared_wrt_key, 6, 0666 | IPC_CREAT);
+        ipcb_control_semaphore(id_wrt, 0, SETVAL, j);
+
+        // trying to get lock(after writing operation done.)
+        ipcb_operate_semaphore(id_wrt, &decrease, 6);
+        ipcb_operate_semaphore(id_wrt, &increase, 6);
+
+        return 0;
+}
