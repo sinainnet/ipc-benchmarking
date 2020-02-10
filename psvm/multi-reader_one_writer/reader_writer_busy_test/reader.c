e8dfeb6ec68e6b721b327037f867f743643b7f4a
Amir Hossein <amirsorouri26@gmail.com>
2020-02-10 18:39:13 +0330

    Busy-waiting model maintained in a way to have a high performance using __synch_add_and_fetch and _synch_val_compare_and_swap c11 primitives.

--- psvm/multi-reader_one_writer/reader_writer_busy_test/reader.c
+++ psvm/multi-reader_one_writer/reader_writer_busy_test/reader.c
@@ -12,6 +12,7 @@
 
 #define THREADS		9
 #define data_len        fourteen_gig_size
+typedef enum {true, false} bool;
 
 
 int main(int argc, char **argv) {
@@ -23,23 +24,19 @@ int main(int argc, char **argv) {
         char *data = calloc(fourteen_gig_row, col);
 
         // Create Shared Memory
-        struct Data *shm = (struct Data*)shm_builder( \
+        struct Data * volatile shm = (struct Data* volatile)shm_builder( \
                 shm_file_creat_mod, shm_prov_prot, \
                 shm_prov_flags, shm_writer_file);
         
         printf("reader: sudo ./writer %d %p %llu\n", \
                 getpid(), data, data_len);
 
+        
         atomic_store(&shm->state, 0);
-        while (atomic_load(&shm->state) != THREADS)
-        {
-                // if (shm->state >= 2)
-                // {
-                //         /* code */
-                //         printf("%d\n", shm->state);
-                // }
-                continue;
-        }
+
+        while (__sync_val_compare_and_swap(&shm->state, THREADS, 0) != THREADS);
+        
+        
         printf("writer just wrote data. I'm done.\n");
 
         /* remove the shared memory object */
