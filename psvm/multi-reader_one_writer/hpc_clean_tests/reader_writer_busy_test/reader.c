3e7f47cdf1de98af007be9c5dadd67b434a2ed46
Amir Hossein Sorouri <amirsorouri26@gmail.com>
2020-02-11 20:09:02 +0330

    make code clean and add automation.

--- psvm/multi-reader_one_writer/hpc_clean_tests/reader_writer_busy_test/reader.c
+++ psvm/multi-reader_one_writer/hpc_clean_tests/reader_writer_busy_test/reader.c
@@ -9,29 +9,32 @@
 
 #include "../../../header.h"
 
-
 #define THREADS		2
 #define data_len        two_gig_size
-typedef enum {true, false} bool;
 
 
 int main(int argc, char **argv) {
-
         // Changing the process scheduling queue into 
         // real-time and set its priority using <sched.h>.
         set_cpu_scheduler(1,99);
 
-        char *data = calloc(two_gig_row, col);
+        char    *data = calloc(two_gig_row, col),
+                *executor_data = calloc(512, sizeof(char));
 
         // Create Shared Memory
         struct Data * volatile shm = (struct Data* volatile)shm_builder( \
                 shm_file_creat_mod, shm_prov_prot, \
                 shm_prov_flags, shm_writer_file);
-        
-        printf("reader: sudo ./writer %d %p %llu\n", \
-                getpid(), data, data_len);
-
         atomic_store(&shm->state, 0);
+        
+        sprintf(executor_data, "./writer %d %p %llu", getpid(), data, data_len);
+        
+        FILE *file_res = fopen(middleware, "w+");
+        fputs(executor_data, file_res);
+
+        printf("reader: sudo ./writer %d %p %llu\n", getpid(), data, data_len);    
+        fclose(file_res);
+        
         while (__sync_val_compare_and_swap(&shm->state, THREADS, 0) != THREADS);
         
         printf("writer just wrote data. I'm done.\n");
--- psvm/multi-reader_one_writer/hpc_clean_tests/reader_writer_busy_test/writer.c
+++ psvm/multi-reader_one_writer/hpc_clean_tests/reader_writer_busy_test/writer.c
@@ -11,37 +11,7 @@
 
 #define THREADS		2
 
-typedef enum {true, false} bool;
-
-static pthread_barrier_t barrier;
-static int numBarriers;
-
-/*
- *  Threads Data Structure to keep track of each one
- */
-typedef struct thread_tags {
-	pthread_t		thread_id;
-	int         		thread_num;
-	struct iovec		*local;
-	struct iovec		*remote;
-	data_input 		input;
-	struct Data		* volatile shm;
-	int 			*lock_count;
-	struct spinlock		*lock;
-} thread_tracker;
-
 thread_tracker thread[THREADS];
-
-typedef struct thread_return_data {
-	struct timespec 	start;
-	struct timespec 	finish;
-	ssize_t 		nread;
-	bool        		printed;
-	bool 			status;
-	int			min_offset;
-	int 			max_offset;
-} thread_result;
-
 
 void* thread_routine (void *arg) {
 	thread_tracker *self = (thread_tracker*) arg;
@@ -51,7 +21,6 @@ void* thread_routine (void *arg) {
 	cpu_set_t set;
 	CPU_ZERO(&set);
 	long long int cpu_no = (self->thread_num%9) + 3;
-//	printf("%lld\n", cpu_no);
 
 	CPU_SET(cpu_no, &set);
 	if (pthread_setaffinity_np(self->thread_id, sizeof(cpu_set_t), &set) == -1)
@@ -80,39 +49,6 @@ void* thread_routine (void *arg) {
 	return (void*)thread_res;
 }
 
-int* calc_max_clock (void **thread2) {
-	thread_result **results = (struct thread_return_data **)thread2;
-	double times [THREADS][2]; 	// [0] for start_time and [1] for end_time
-	
-	for (int i = 0; i < THREADS; i++)
-	{
-		times[i][0] = (double)results[i]->start.tv_sec + ((double)results[i]->start.tv_nsec/(double)1000000000);
-		times[i][1] = (double)results[i]->finish.tv_sec + ((double)results[i]->finish.tv_nsec/(double)1000000000);
-	}
-
-	int 	finish_offset_max = 0;
-	int 	start_offset_min = 0;
-	double 	finish = times[0][1];
-	double 	start = times[0][0];
-	for (int i = 1; i < THREADS; i++)
-	{
-		if (times[i][0] < start)
-		{
-			start_offset_min = i;
-			start = times[i][0];
-		}
-		if (times[i][1] > finish)
-		{
-			finish_offset_max = i;
-			finish = times[i][1];
-		}
-	}
-	int *offset = (int *)calloc(2, sizeof(int));
-	offset[0] = start_offset_min;		// [0]  for start_time
-	offset[1] = finish_offset_max;		// [1]	for finish_time
-
-	return offset;
-}
 
 int main (int argc, char **argv) {
 	set_cpu_scheduler(2, 99);
@@ -196,7 +132,7 @@ int main (int argc, char **argv) {
 		}
 	}
 
-	int *offsets = calc_max_clock((void **)all_threads);
+	int *offsets = calc_max_clock((void **)all_threads, THREADS);
 	struct timespec start = all_threads[offsets[0]]->start;
 	struct timespec finish = all_threads[offsets[1]]->finish;
 	ssize_t nreads = 0;
